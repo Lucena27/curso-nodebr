e14963e60c3b5efd2884178dc7bd55fe3652fcf3
Lucena <lucas.lucena@g4flex.com.br>
2019-07-17 16:27:08 -0300

    Async/Await

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -52,15 +52,25 @@ function obterEndereco(idUsuario, callback) {
 main()
 async function main(){
     try{
+        console.time('medida-promise')
         const usuario = await obterUsuario()
-        const telefone = await obterTelefone(usuario.id)
-        const endereco = await obterEnderecoAsync(usuario.id)
+        // const telefone = await obterTelefone(usuario.id)
+        // const endereco = await obterEnderecoAsync(usuario.id)
+
+        const resultado = await Promise.all([
+            obterTelefone(usuario.id),
+            obterEnderecoAsync(usuario.id)
+        ])
+
+        const endereco = resultado[1]
+        const telefone = resultado[0]
 
         console.log(`
             Nome: ${usuario.nome},
             Telefone: (${telefone.ddd}) ${telefone.telefone},
             Endereco: ${endereco.rua}, ${endereco.numero}
         `)
+        console.timeEnd('medida-promise')
     }catch(error){
         console.log('Deu ruim aqui irmão', error)
     }
